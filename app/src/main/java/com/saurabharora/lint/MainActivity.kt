3d7589f20b071a015c9ff167cfd59d98099536b7
Saurabh Arora <saurabharora90@gmail.com>
2019-07-30 01:07:21 +0800

    push sample app which has lint issues

---
+++ app/src/main/java/com/saurabharora/lint/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.saurabharora.lint
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
